59c7ac471b675920a9a64971a37ce48675948345
Michael Fogleman <fogleman@gmail.com>
2014-08-06 20:43:00 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pg',
+    version='0.1',
+    description='Python Distribution Utilities',
+    author='Michael Fogleman',
+    author_email='fogleman@gmail.com',
+    url='https://github.com/fogleman/pg',
+    packages=['pg'],
+)
