55ae6380bdf6ad8b14205f1c8b834ce474fafde4
Michael Fogleman <fogleman@gmail.com>
2014-08-18 16:24:19 -0400

    cleanup

--- examples/pipes.py
+++ examples/pipes.py
@@ -3,18 +3,20 @@ import random
 
 PIPES = 8
 SIZE = 16
+TURN_PROBABILITY = 0.3
 UPDATE_RATE = 0.05
 RESTART_RATE = 30
 
 DIRECTIONS = [
-    (-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1),
+    (0, -1, 0, 0), (0, 1, 0, 0),
+    (1, 0, -1, 0), (1, 0, 1, 0),
+    (2, 0, 0, -1), (2, 0, 0, 1),
 ]
 
 COLORS = [
-    0x1f77b4, 0xaec7e8, 0xff7f0e, 0xffbb78, 0x2ca02c,
-    0x98df8a, 0xd62728, 0xff9896, 0x9467bd, 0xc5b0d5,
-    0x8c564b, 0xc49c94, 0xe377c2, 0xf7b6d2, 0x7f7f7f,
-    0xc7c7c7, 0xbcbd22, 0xdbdb8d, 0x17becf, 0x9edae5,
+    0x1f77b4, 0xaec7e8, 0xff7f0e, 0xffbb78, 0x2ca02c, 0x98df8a,
+    0xd62728, 0xff9896, 0x9467bd, 0xc5b0d5, 0x8c564b, 0xc49c94,
+    0xe377c2, 0xf7b6d2, 0x7f7f7f, 0xc7c7c7, 0x17becf, 0x9edae5,
 ]
 
 SPHERE = pg.Sphere(2, 0.25, (0, 0, 0))
@@ -56,22 +58,19 @@ class Pipe(object):
         x, y, z = self.position
         directions = list(DIRECTIONS)
         random.shuffle(directions)
-        if random.random() < 0.7:
+        if random.random() > TURN_PROBABILITY:
             directions.remove(self.direction)
             directions.insert(0, self.direction)
         for direction in directions:
-            dx, dy, dz = direction
+            axis, dx, dy, dz = direction
             nx, ny, nz = x + dx, y + dy, z + dz
-            if nx < -SIZE or ny < -SIZE or nz < -SIZE:
-                continue
-            if nx > SIZE or ny > SIZE or nz > SIZE:
-                continue
             if (nx, ny, nz) in self.occupied:
+                continue
+            if any(n < -SIZE or n > SIZE for n in (nx, ny, nz)):
                 continue
             self.position = (nx, ny, nz)
             self.occupied.add(self.position)
             mx, my, mz = x + dx / 2.0, y + dy / 2.0, z + dz / 2.0
-            axis = map(bool, direction).index(True)
             add_cylinder(self.vb, (mx, my, mz), axis)
             if direction != self.direction:
                 add_sphere(self.vb, (x, y, z))
@@ -101,8 +100,7 @@ class Window(pg.Window):
             for pipe in self.pipes:
                 pipe.update()
     def draw(self):
-        matrix = pg.Matrix()
-        matrix = self.wasd.get_matrix(matrix)
+        matrix = self.wasd.get_matrix()
         matrix = matrix.perspective(65, self.aspect, 0.1, 1000)
         self.clear()
         for pipe in self.pipes:
