538331c744b56d94e9fdfdba904ec3774b7a2272
Michael Fogleman <fogleman@gmail.com>
2014-08-18 22:21:01 -0400

    cleanup

--- examples/starfield.py
+++ examples/starfield.py
@@ -13,17 +13,17 @@ class Window(pg.Window):
         for _ in xrange(COUNT):
             x = (random.random() - 0.5) * FIELD_SIZE
             y = (random.random() - 0.5) * FIELD_SIZE
-            z = (random.random()) * FIELD_DEPTH
+            z = random.random() * FIELD_DEPTH
             mesh = pg.Matrix().translate((x, y, z)) * shape
             data.extend(pg.interleave(mesh.positions, mesh.normals))
         self.vb = pg.VertexBuffer(data)
         self.context = pg.Context(pg.DirectionalLightProgram())
-        self.context.light_direction = pg.normalize((-1, 1, 1))
+        self.context.light_direction = (0, 0, 1)
         self.context.object_color = (1, 1, 1)
         self.context.position, self.context.normal = self.vb.slices(3, 3)
     def draw(self):
         self.clear()
-        for m in range(-1, 2):
+        for m in xrange(-1, 2):
             z = m * FIELD_DEPTH + (-self.time * SPEED) % FIELD_DEPTH
             matrix = pg.Matrix().translate((0, 0, -z))
             matrix = matrix.perspective(65, self.aspect, 1, 500)
