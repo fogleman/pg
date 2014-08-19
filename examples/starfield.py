6a5b72d31c738fb84f1e7089ec822d8a154dd51f
Michael Fogleman <fogleman@gmail.com>
2014-08-18 22:18:24 -0400

    starfield example

---
+++ examples/starfield.py
@@ -0,0 +1,35 @@
+import pg
+import random
+
+SPEED = 250
+COUNT = 2500
+FIELD_SIZE = 500
+FIELD_DEPTH = 2500
+
+class Window(pg.Window):
+    def setup(self):
+        data = []
+        shape = pg.Plane((0, 0, 0), (0, 0, 1), 0.5, False)
+        for _ in xrange(COUNT):
+            x = (random.random() - 0.5) * FIELD_SIZE
+            y = (random.random() - 0.5) * FIELD_SIZE
+            z = (random.random()) * FIELD_DEPTH
+            mesh = pg.Matrix().translate((x, y, z)) * shape
+            data.extend(pg.interleave(mesh.positions, mesh.normals))
+        self.vb = pg.VertexBuffer(data)
+        self.context = pg.Context(pg.DirectionalLightProgram())
+        self.context.light_direction = pg.normalize((-1, 1, 1))
+        self.context.object_color = (1, 1, 1)
+        self.context.position, self.context.normal = self.vb.slices(3, 3)
+    def draw(self):
+        self.clear()
+        for m in range(-1, 2):
+            z = m * FIELD_DEPTH + (-self.time * SPEED) % FIELD_DEPTH
+            matrix = pg.Matrix().translate((0, 0, -z))
+            matrix = matrix.perspective(65, self.aspect, 1, 500)
+            self.context.matrix = matrix
+            self.context.camera_position = (0, 0, z)
+            self.context.draw()
+
+if __name__ == "__main__":
+    pg.run(Window)
