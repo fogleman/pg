519c741976ff48974fec04986e38c5749ad4ec99
Michael Fogleman <fogleman@gmail.com>
2014-09-05 10:02:54 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pg',
     version='0.1',
     description='Python OpenGL Graphics Framework',
     author='Michael Fogleman',
-    author_email='fogleman@gmail.com',
+    author_email='michael.fogleman@gmail.com',
     url='https://github.com/fogleman/pg',
     packages=['pg'],
     install_requires=[
-        'glfw',
         'Pillow',
         'PyOpenGL',
     ],
