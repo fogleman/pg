7a2c284e65a24ea873dbf382a9c6769dbb8c6e83
Michael Fogleman <fogleman@gmail.com>
2014-09-06 22:45:57 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='pg',
     version='0.1',
     description='Python OpenGL Graphics Framework',
+    license='MIT',
     author='Michael Fogleman',
     author_email='michael.fogleman@gmail.com',
     url='https://github.com/fogleman/pg',
@@ -11,5 +12,14 @@ setup(
     install_requires=[
         'Pillow',
         'PyOpenGL',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Graphics :: 3D Rendering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ],
 )
